4a4eba3fb6b6203106e8c9683862e00ec0cd30a8
VictoralcNunes <victoralcnunes@hotmail.com>
2020-08-26 22:46:29 -0300

    Game over and restart

--- js/snake.js
+++ js/snake.js
@@ -8,7 +8,7 @@ snake[0] = {
     y: 8*box
 } 
 
-let direction = "left";
+let direction = "right";
 
 let food= {
     x: Math.floor(Math.random()*15 + 1) * box,
@@ -32,6 +32,19 @@ function drawFood(){
     context.fillRect(food.x, food.y, box, box);
 }
 
+function restart(){
+    snake = [];
+    snake[0] = {
+        x: 8*box,
+        y: 8*box
+    } 
+    food= {
+        x: Math.floor(Math.random()*15 + 1) * box,
+        y: Math.floor(Math.random()*15 + 1) * box 
+    }
+    direction = "right";
+}
+
 document.addEventListener('keydown', update);
 
 // muda direção
@@ -49,6 +62,13 @@ function iniciarJogo(){
     if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16*box;
 
+    for (i = 1; i < snake.length; i++) {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Game Over...")
+        }
+    }
+
     criarBG();
     criarCobra();
     drawFood();
@@ -61,7 +81,14 @@ function iniciarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{
+        food.x= Math.floor(Math.random()*15 + 1) * box;
+        food.y= Math.floor(Math.random()*15 + 1) * box;
+        
+    }
 
     let newHead = {
         x: snakeX,
